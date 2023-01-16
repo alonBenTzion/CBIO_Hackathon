ab8f00be52eed3549156bdf30f20a48948a2fe8f
AlonBenZion <alon@genetikaplus.com>
2023-01-16 09:18:33 +0200

    test model

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from hmmlearn import hmm
 import gzip as gz
 import pandas as pd
 import numpy as np
+import pickle
 
 NUM_OF_STATS = 3
 STATES = {"H": 0, "S": 1, "L": 2}
@@ -14,14 +15,14 @@ AMINO_ACIDS = {"A": 0, "R": 1, "N": 2, "D": 3, "C": 4, "Q": 5, "E": 6, "G": 7, "
 BAD_AMINO_ACIDS = "OUBZXJ"
 EMISSIONS = pd.read_csv("emissions-Table 1.csv", index_col=0).T.to_numpy() / 100
 TRANSITIONS = np.array([[12 / 13, 1 / 39, 2 / 39], [1 / 15, 4 / 5, 2 / 15], [0.4, 0.2, 0.4]])
-
+PATH_TO_DATA = "/Users/alonbentzion/Downloads/parsed_sequences" # Local
 
 def split_train_test(dir_path: str):
     train_samples, train_lengths = [], []
     test_samples, test_lengths, test_labels = [], [], []
 
     for file in os.listdir(dir_path):
-        flag = np.random.binomial(1, 0.5, 1)[0]
+        flag = np.random.binomial(1, 0.8, 1)[0]
         file = f"{dir_path}/{file}"
         with open(file, 'r') as f:
             content = f.readlines()
@@ -42,7 +43,7 @@ def split_train_test(dir_path: str):
     return train_samples, train_lengths, test_samples, test_lengths, test_labels
 
 
-def train(train_samples, train_lengths, convergence_threshold: float = 0.01, num_iters: int = 120):
+def train(train_samples, train_lengths, convergence_threshold: float = 0.01, num_iters: int = 80):
     # Create HMM model
     model = hmm.CategoricalHMM(n_components=NUM_OF_STATS,
                                algorithm='viterbi',
@@ -63,17 +64,26 @@ def train(train_samples, train_lengths, convergence_threshold: float = 0.01, num
 
 def main():
     # Get train and test samples
-    train_samples, train_lengths, test_samples, test_lengths, test_labels = split_train_test("data")
+    train_samples, train_lengths, test_samples, test_lengths, test_labels = split_train_test()
 
-    # Create and train model
-    model = train(train_samples, train_lengths)
+    errors_rate = []
+    for iter in range(1, 100, 2):
+        # Create and train model
+        model = train(train_samples, train_lengths, num_iters=iter)
 
-    # Predict
-    misclassification_error = 0
-    for sample, length, label in zip(test_samples, test_lengths, test_labels):
-        ll, hidden_states = model.decode(sample, length)
-        misclassification_error += np.count_nonzero(hidden_states - label)
-    misclassification_error_rate = misclassification_error / np.sum(test_lengths)
+        # Predict
+        misclassification_error = 0
+        for sample, length, label in zip(test_samples, test_lengths, test_labels):
+            ll, hidden_states = model.decode(sample, length)
+            misclassification_error += np.count_nonzero(hidden_states - label)
+        # Save misclassification error
+        misclassification_error_rate = misclassification_error / np.sum(test_lengths)
+        errors_rate.append(misclassification_error_rate)
+
+        # Create pickle file
+        with open(f"models/model_{iter}", 'wb') as file:
+            pickle.dump(model, file)
+
 
 
 if __name__ == '__main__':
